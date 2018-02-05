986ced35d0f86ccd5a33741170d1089e7e542b02
kaushiosu <kaushik2013@gmail.com>
2018-02-05 01:26:42 -0600

    display click count for each image.

--- catClickerPremium.js
+++ catClickerPremium.js
@@ -8,22 +8,25 @@ var urlId = 20785;
 for (var i = 0; i < numberOfCatPictures; i++){
   var urlReplacement = urlId + i;
   var targetUrl = catPictureUrlBase.replace(/\*/g,urlReplacement);
-  console.log('Target url is '+ targetUrl);
   var element = document.createElement('div');
   //element.href = targetUrl;
+  var elementClickCount = 0;
   element.textContent = targetUrl;
   element.addEventListener('click',(function(targetUrl,element){
     var image = document.createElement('img');
     image.style.display="none";
     image.src = targetUrl;
     image.clickCount = 0;
-    image.addEventListener('click',function(){
-      this.clickCount++;
-      console.log("image "+ this.src + " clicked "+ this.clickCount+ " times");
-      if(this.clickCount == 5){
-        this.style.display = "none";
-      }
-    },false);
+    image.addEventListener('click',(function(parent){
+      return function(){
+        this.clickCount++;
+        parent.textContent = this.src + " : "+ (this.clickCount);
+        if(this.clickCount == 5){
+          this.clickCount = 0;
+          this.style.display = "none";
+        }
+      };
+    })(element),false);
     document.body.appendChild(image);
     return function(){
       if (image.style.display === "none")
@@ -32,6 +35,6 @@ for (var i = 0; i < numberOfCatPictures; i++){
         image.style.display= "none";
       }
     };
-  })(targetUrl,this),false);
+  })(targetUrl,element),false);
   document.body.appendChild(element);
 }
